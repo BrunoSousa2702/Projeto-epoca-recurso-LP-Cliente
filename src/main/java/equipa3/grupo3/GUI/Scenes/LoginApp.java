745e14ddd1a1f7f9c9258ec3e90ee2beee8c6909
mariagomesf3 <50412@alunos.upt.pt>
2025-02-03 18:45:08 +0000

    Update LoginApp.java

--- src/main/java/equipa3/grupo3/GUI/Scenes/LoginApp.java
+++ src/main/java/equipa3/grupo3/GUI/Scenes/LoginApp.java
@@ -1,33 +1,28 @@
 package equipa3.grupo3.GUI.Scenes;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
-import equipa3.grupo3.services.ApiService;
 
 public class LoginApp extends Application {
 
-
-
-    
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("equipa3/grupo3/GUI/Scenes/menuLogin.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("tabela_tarefas.fxml"));
             Parent root = loader.load();
             
             Scene scene = new Scene(root);
-            primaryStage.setTitle("Login - To Do List");
+            primaryStage.setTitle("Tabela de Tarefas");
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
